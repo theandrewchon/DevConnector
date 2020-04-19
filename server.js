5eb6d06fc92d64cfd65e4600bd01930779c1769c
Andrew Chon <chonclan@gmail.com>
2020-04-19 13:00:30 -0500

    inital commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+//Global variables
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('API Running');
+});
+
+app.listen(PORT, () =>
+  console.log(
+    `Server started on port ${PORT}. Link is http://localhost:${PORT}`
+  )
+);
