c14d2b18de1d40fc565e42e1d69af7895b21e7a7
Andrew Chon <chonclan@gmail.com>
2020-04-19 13:26:10 -0500

    config values

---
+++ config/db.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    /* Connection to MongoDB Atlas cloud server */
+    // await mongoose.connect(db, {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // });
+    /* Connection for Heroku or Local MongoDB Server */
+    await mongoose.connect(
+      process.env.MONGODB_URI || 'mongodb://localhost/BAEdb',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log('---------- \n MongoDB connected\n----------');
+  } catch (err) {
+    console.error(err.message);
+    //Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 //Global variables
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+//Connect to Mongo DB
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('API Running');
