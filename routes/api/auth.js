a553b7de2b063118cf2e9218f745f3e528edbfed
Andrew Chon <chonclan@gmail.com>
2020-04-20 20:34:01 -0500

    auth middleware and JWT verification

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,19 @@
 const express = require('express');
-
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const db = require('../../models');
 
 //@route    GET api/auth
 //@desc     Test Route
 //@access   Public
-router.get('/', (req, res) => res.send('Auth Route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await await db.User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.err(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
