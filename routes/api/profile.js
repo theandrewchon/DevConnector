4acd5083c3f1bf33cf567bbe30d12e7e5ab30d45
Andrew Chon <chonclan@gmail.com>
2020-05-20 21:03:52 -0500

    get github pages

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,8 @@ const auth = require('../../middleware/auth');
 const db = require('../../models');
 const { check, validationResult } = require('express-validator');
 const normalize = require('normalize-url');
+const axios = require('axios');
+const config = require('config');
 const router = express.Router();
 
 //@route    GET api/profile/me
@@ -306,6 +308,28 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
+  }
+});
+
+//@route    GET api/profile/github:username
+//@desc     Get user repos from Github
+//@access   Public
+
+router.get('/github/:username', async (req, res) => {
+  try {
+    const uri = encodeURI(
+      `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+    );
+    const headers = {
+      'user-agent': 'node.js',
+      Authorization: `token ${config.get('githubToken')}`,
+    };
+
+    const gitHubResponse = await axios.get(uri, { headers });
+    return res.json(gitHubResponse.data);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
   }
 });
 
