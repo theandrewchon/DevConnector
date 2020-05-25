fd4ed0dbb80e3cdd051568aa357bfdef5ba003ce
Andrew Chon <chonclan@gmail.com>
2020-05-24 20:48:29 -0500

    alert reducer/action

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,12 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
 import './App.css';
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
+
 function App() {
   return (
     <Provider store={store}>
@@ -16,6 +18,7 @@ function App() {
           <Navbar />
           <Route exact path="/" component={Landing} />
           <section className="container">
+            <Alert />
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-export default function Register() {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -17,31 +20,25 @@ export default function Register() {
   const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log('Passwords do not match');
+      setAlert('Passwords do not match', 'danger');
     } else {
-      console.log('success');
     }
   };
 
   return (
-    <>
+    <Fragment>
       <h1 className="large text-primary">Sign Up</h1>
       <p className="lead">
-        <i className="fas fa-user"></i> Create Your Account
+        <i className="fas fa-user" /> Create Your Account
       </p>
-      <form
-        className="form"
-        action="create-profile.html"
-        onSubmit={(e) => onSubmit(e)}
-      >
+      <form className="form" onSubmit={onSubmit}>
         <div className="form-group">
           <input
             type="text"
             placeholder="Name"
             name="name"
-            required
-            onChange={(e) => onChange(e)}
             value={name}
+            onChange={onChange}
           />
         </div>
         <div className="form-group">
@@ -49,9 +46,8 @@ export default function Register() {
             type="email"
             placeholder="Email Address"
             name="email"
-            onChange={(e) => onChange(e)}
             value={email}
-            required
+            onChange={onChange}
           />
           <small className="form-text">
             This site uses Gravatar so if you want a profile image, use a
@@ -63,9 +59,8 @@ export default function Register() {
             type="password"
             placeholder="Password"
             name="password"
-            minLength="6"
-            onChange={(e) => onChange(e)}
             value={password}
+            onChange={onChange}
           />
         </div>
         <div className="form-group">
@@ -73,9 +68,8 @@ export default function Register() {
             type="password"
             placeholder="Confirm Password"
             name="password2"
-            minLength="6"
-            onChange={(e) => onChange(e)}
             value={password2}
+            onChange={onChange}
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
@@ -83,6 +77,12 @@ export default function Register() {
       <p className="my-1">
         Already have an account? <Link to="/login">Sign In</Link>
       </p>
-    </>
+    </Fragment>
   );
-}
+};
+
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setAlert })(Register);
