b1b57f1b22a3d79347583b9f54054aa39b221d59
Andrew Chon <chonclan@gmail.com>
2020-06-01 22:11:50 -0500

    create profile form

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -18,33 +18,33 @@ const Dashboard = ({
   }, [getCurrentProfile]);
 
   return (
-    <div>Hi</div>
-    // <Fragment>
-    //   <h1 className="large text-primary">Dashboard</h1>
-    //   <p className="lead">
-    //     <i className="fas fa-user" /> Welcome {user && user.name}
-    //   </p>
-    //   {profile !== null ? (
-    //     <Fragment>
-    //       <DashboardActions />
-    //       <Experience experience={profile.experience} />
-    //       <Education education={profile.education} />
+    <Fragment>
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Welcome {user && user.name}
+      </p>
+      {profile !== null ? (
+        <div>{user.name}</div>
+      ) : (
+        // <Fragment>
+        //   <DashboardActions />
+        //   <Experience experience={profile.experience} />
+        //   <Education education={profile.education} />
 
-    //       <div className="my-2">
-    //         <button className="btn btn-danger" onClick={() => deleteAccount()}>
-    //           <i className="fas fa-user-minus" /> Delete My Account
-    //         </button>
-    //       </div>
-    //     </Fragment>
-    //   ) : (
-    //     <Fragment>
-    //       <p>You have not yet setup a profile, please add some info</p>
-    //       <Link to="/create-profile" className="btn btn-primary my-1">
-    //         Create Profile
-    //       </Link>
-    //     </Fragment>
-    //   )}
-    // </Fragment>
+        //   <div className="my-2">
+        //     <button className="btn btn-danger" onClick={() => deleteAccount()}>
+        //       <i className="fas fa-user-minus" /> Delete My Account
+        //     </button>
+        //   </div>
+        // </Fragment>
+        <Fragment>
+          <p>You have not yet setup a profile, please add some info</p>
+          <Link to="/create-profile" className="btn btn-primary my-1">
+            Create Profile
+          </Link>
+        </Fragment>
+      )}
+    </Fragment>
   );
 };
 
--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -4,7 +4,7 @@ import Register from '../auth/Register';
 import Login from '../auth/Login';
 import Alert from '../layout/Alert';
 import Dashboard from '../dashboard/Dashboard';
-// import ProfileForm from '../profile-forms/ProfileForm';
+import ProfileForm from '../profile-forms/ProfileForm';
 // import AddExperience from '../profile-forms/AddExperience';
 // import AddEducation from '../profile-forms/AddEducation';
 // import Profiles from '../profiles/Profiles';
@@ -24,7 +24,7 @@ const Routes = (props) => {
         {/* <Route exact path="/profiles" component={Profiles} /> */}
         {/* <Route exact path="/profile/:id" component={Profile} /> */}
         <PrivateRoute exact path="/dashboard" component={Dashboard} />
-        {/* <PrivateRoute exact path="/create-profile" component={ProfileForm} /> */}
+        <PrivateRoute exact path="/create-profile" component={ProfileForm} />
         {/* <PrivateRoute exact path="/edit-profile" component={ProfileForm} /> */}
         {/* <PrivateRoute exact path="/add-experience" component={AddExperience} /> */}
         {/* <PrivateRoute exact path="/add-education" component={AddEducation} /> */}
