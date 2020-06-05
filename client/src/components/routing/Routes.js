d81f4a9114bc6613a98194f365e29262ac49cb95
Andrew Chon <andrewdchon@gmail.com>
2020-06-04 22:06:48 -0500

    404 page, prepared for deployment

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -11,7 +11,7 @@ import Profiles from '../profiles/Profiles';
 import Profile from '../profile/Profile';
 import Posts from '../posts/Posts';
 import Post from '../post/Post';
-// import NotFound from '../layout/NotFound';
+import NotFound from '../layout/NotFound';
 import PrivateRoute from '../routing/PrivateRoute';
 
 const Routes = (props) => {
@@ -28,9 +28,9 @@ const Routes = (props) => {
         <PrivateRoute exact path="/edit-profile" component={ProfileForm} />
         <PrivateRoute exact path="/add-experience" component={AddExperience} />
         <PrivateRoute exact path="/add-education" component={AddEducation} />
-        <PrivateRoute exact path="/posts" component={Posts} /> */}
-        {/* <PrivateRoute exact path="/posts/:id" component={Post} />
-        {/* <Route component={NotFound} /> */}
+        <PrivateRoute exact path="/posts" component={Posts} />
+        <PrivateRoute exact path="/posts/:id" component={Post} />
+        <Route component={NotFound} />
       </Switch>
     </section>
   );
